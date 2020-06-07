c0b1c05d9c8e65d248037a17abd395386b5111b8
Will Nielsen <wjn@MacBook-Pro.local>
2020-06-07 08:39:47 -0500

    mongoose implementation

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://wjn:DfWNJXLjrFYL3JDMJJ2H@cluster0-njk2r.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+  // options object
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+db.on('error', (err) => {
+  console.log('[Error connecting to mongo] : ', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
